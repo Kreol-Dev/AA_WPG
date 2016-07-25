50d06ddda4a5a69c043fd03e39408821b6c85916
Виталий Буланенков <vitaly.bulanenkov@gmail.com>
2016-07-25 22:36:59 +0300

    Cols fix

--- AA_WPG/Clusters/States.cs
+++ AA_WPG/Clusters/States.cs
@@ -3,9 +3,20 @@ namespace AA_WPG
 {
 	public class States : RemoteDataCluster
 	{
+		public float BaseStateFactoryTaxPercent;
+		public float BaseInvestements;
+
 		public override void Init(GoogleDataUpdater updater)
 		{
-			throw new NotImplementedException();
+			var statesIDs = updater.GetSheetRange("States", "A", 2, "Z", 3,"1NSBFZxNsfgRmbgaQIj0lYHzUliNOS2bwPxOqs4bj3gM");
+
+			var consumptionTable = updater.GetSheetRange("States", "A", 5, "Z", 8, "1NSBFZxNsfgRmbgaQIj0lYHzUliNOS2bwPxOqs4bj3gM");
+
+
+			var variablesTable = updater.GetSheetRange("States", "A", 10, "A", 12, "1NSBFZxNsfgRmbgaQIj0lYHzUliNOS2bwPxOqs4bj3gM");
+
+
+
 		}
 
 		public override void Upload(GoogleDataUpdater updater)
@@ -17,6 +28,11 @@ namespace AA_WPG
 	public class State
 	{
 		public string Name;
+		public int Population;
+		public float Tax;
+		public float Corruption;
+		public float EconomyEfficiency;
+		public float StateEfficiency;
 	}
 
 	public abstract class StateModifier
---
+++ AA_WPG/Commands/LoadStates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AA_WPG
+{
+	public class LoadStates : ConsoleCommand
+	{
+		public override void Execute(string[] args)
+		{
+			
+		}
+	}
+}
+
